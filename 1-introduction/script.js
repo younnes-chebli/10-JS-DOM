f87c26b10127a085fe7bd5c6e5e7f57033e78f06
Younnes Chebli <younnesc@gmail.com>
2022-10-17 11:02:53 +0200

    added intro

---
+++ 1-introduction/script.js
@@ -0,0 +1,17 @@
+let title = document.title;
+console.log(title);
+
+title = "Modifying the DOM";
+console.log(title);
+
+let body = document.body;
+//body.style.backgroundColor = "#FF69B4";
+let a = Math.floor(Math.random() * 254);
+let b = Math.floor(Math.random() * 254);
+let c = Math.floor(Math.random() * 254);
+body.style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
+
+let bodyChildren = document.body.children;
+for(child of bodyChildren) {
+    child.style.display = "";
+}
